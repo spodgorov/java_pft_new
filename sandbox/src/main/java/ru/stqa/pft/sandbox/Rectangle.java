d74e9507f7857302cb85168747c36c47a86e6647
sp2017sp <podgorov@gmail.com>
2017-10-18 00:25:28 +0300

    New Tests with Rectangle and Square

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Sergey.Podgorov on 10/17/2017.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle (double a, double b) {
+    this.a = a;
+    this.b = b;
+
+
+  }
+
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Sergey.Podgorov on 10/17/2017.
+ */
+public class Square {
+  public double l;
+
+  public Square(double l) {
+    this.l = l; //инициализация нового объекта. Заполнение его атрибута какими-то значениями.
+  }
+}
